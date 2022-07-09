f42a781fdce64026d5e6e85bda5edababdf53314
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-07-09 18:31:34 -0500

    made the code examples

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,30 @@
+import Link from 'next/link';
+
 function Home() {
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Welcome Home! </h1>
-    </div>
+    <>
+      <h1>Dis da home page!</h1>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/events">Events</Link>
+        </li>
+        <li>
+          <Link href="/props">Props</Link>
+        </li>
+        <li>
+          <Link href="/hooks">Hooks</Link>
+        </li>
+        <li>
+          <Link href="/routes">Routes</Link>
+        </li>
+        <li>
+          <Link href="/forms">Forms</Link>
+        </li>
+      </ul>
+    </>
   );
 }
 
